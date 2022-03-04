99ccc781fbacb7acee80deb7cbcdc11b30abc080
katbelowaverage <93794876+katbelowaverage@users.noreply.github.com>
2022-03-04 15:27:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function myFunction() {
-  var x = document.getElementById("myText1").value;
+var k = document.getElementById("input").value;
+var r = "";
+for(let i = k.length-1; i >= 0; i--){
+	r += String.fromCharCode(
+    (k.charCodeAt(i)-
+    "AnyaChenQT".charAt((k.length-i-1)%10).charCodeAt(0))
+    /("ilysm".charAt((k.length-i-1)%5).charCodeAt(0)-96));
+}
+var x = document.getElementById("myText1").value;
   var y = document.getElementById("myText2").value;
   var z = document.getElementById("myText3").value;
-  var s = (x + ", " + y + ", " + z + ": ") + (x*x<y&&y<z*z) + "<br>";
+  var s = (x + ", " + y + ", " + z + ": ") + eval(r) + "<br>";
   document.getElementById("demo").innerHTML = s + document.getElementById("demo").innerHTML;
+  }
+  //x*x<y&&y<z*z
+  function myFunction2() {
+  	var k = document.getElementById("input2").value;
+var r = "";
+for(let i = k.length-1; i >= 0; i--){
+	r += String.fromCharCode(
+    k.charCodeAt(i)*
+    ("ilysm".charAt(i%5).charCodeAt(0)-96)
+    +"AnyaChenQT".charAt(i%10).charCodeAt(0))
 }
+  document.getElementById("demo2").innerHTML = r;
+
+  }
