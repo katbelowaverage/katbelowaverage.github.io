9acc24b0a1e1ddfe214f0fffe01f9833a5027a37
katbelowaverage <93794876+katbelowaverage@users.noreply.github.com>
2022-03-04 13:15:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function myFunction() {
   var x = document.getElementById("myText1").value;
   var y = document.getElementById("myText2").value;
   var z = document.getElementById("myText3").value;
-  var s = (x + ", " + y + ", " + z + ": ") + ((x*y)%z == 0) + "<br>";
+  var s = (x + ", " + y + ", " + z + ": ") + (x*x<y&&y<z*z) + "<br>";
   document.getElementById("demo").innerHTML = s + document.getElementById("demo").innerHTML;
 }
